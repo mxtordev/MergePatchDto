707191054b82f202fcd4ca14af4ea294fe1a3fb9
mxtordev <64542491+mxtordev@users.noreply.github.com>
2026-07-05 10:16:20 +0200

    Add package metadata license and API docs

--- src/MergePatchDto/MergePatchTargetAttribute.cs
+++ src/MergePatchDto/MergePatchTargetAttribute.cs
@@ -2,14 +2,24 @@ using System;
 
 namespace MergePatch
 {
+    /// <summary>
+    /// Adds an additional target type for a merge-patch DTO.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public sealed class MergePatchTargetAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a merge-patch target declaration for the specified type.
+        /// </summary>
+        /// <param name="targetType">The additional type that receives generated apply methods.</param>
         public MergePatchTargetAttribute(Type targetType)
         {
             TargetType = targetType;
         }
 
+        /// <summary>
+        /// Gets the additional target type for generated apply methods.
+        /// </summary>
         public Type TargetType { get; }
     }
 }
--- src/MergePatchDto/PatchUsingAttribute.cs
+++ src/MergePatchDto/PatchUsingAttribute.cs
@@ -2,14 +2,24 @@ using System;
 
 namespace MergePatch
 {
+    /// <summary>
+    /// Uses a named mapping method when applying a patch DTO property to a target.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public sealed class PatchUsingAttribute : Attribute
     {
+        /// <summary>
+        /// Initializes a custom apply-method mapping.
+        /// </summary>
+        /// <param name="methodName">The method name used to apply this patch property.</param>
         public PatchUsingAttribute(string methodName)
         {
             MethodName = methodName;
         }
 
+        /// <summary>
+        /// Gets the method name used to apply this patch property.
+        /// </summary>
         public string MethodName { get; }
     }
 }
